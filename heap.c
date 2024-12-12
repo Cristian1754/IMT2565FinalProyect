3e67d2d7595e7158d49e51ee9ec2342933dc506f
Cristian1754 <cristian.chvez@uc.cl>
2024-12-12 19:03:35 -0300

    Heaps debugged

--- heap.c
+++ heap.c
@@ -34,15 +34,21 @@ NODE *extractMin(HEAP *heap)
     heap->array[0] = NULL;
     swap(heap, 0, heap->last - 1);
     heap->last = heap->last - 1;
+    siftDown(heap, 0);
     return min_node;
 }
 
 void swap(HEAP *heap, int index1, int index2)
 {
+    if (index1 == index2) { return;}
+
     NODE *node1 = heap->array[index1];
     NODE *node2 = heap->array[index2];
     heap->array[index1] = node2;
     heap->array[index2] = node1;
+
+    node1->heap_index = index2;
+    node2->heap_index = index1;
 }
 
 void siftUp(HEAP *heap, int index)
@@ -61,6 +67,36 @@ void siftUp(HEAP *heap, int index)
         swap(heap, index, parent_index);
         siftUp(heap, parent_index);
     }
+}
+
+void siftDown(HEAP* heap, int index) {
+
+    int lchild_index = (2*index) + 1;
+    int rchild_index = (2*index) + 2;
+    NODE* min_child_node;
+    int min_child_index;
+
+    if (lchild_index >= heap->size || rchild_index >= heap->size){ return;}
+    if (heap->array[lchild_index] == NULL) { return;}
+
+    if (heap->array[rchild_index] == NULL){
+        min_child_node = heap->array[lchild_index];
+        min_child_index = lchild_index;
+    } else {
+        if (heap->array[lchild_index]->cost < heap->array[rchild_index]->cost) {
+            min_child_node = heap->array[lchild_index];
+            min_child_index = lchild_index;
+        } else {
+            min_child_node = heap->array[rchild_index];
+            min_child_index = rchild_index;
+        }
+    }
+
+    if (heap->array[index]->cost < min_child_node->cost) {
+        swap(heap, index, min_child_index);
+        siftDown(heap, min_child_index);
+    }
+
 }
 
 void decreaseKey(HEAP *heap, int index, int new_cost)
--- heap.h
+++ heap.h
@@ -15,4 +15,5 @@ void insertNode(HEAP* heap, NODE* node);
 NODE* extractMin(HEAP* heap);
 void swap(HEAP* heap, int index1, int index2);
 void siftUp(HEAP* heap, int index);
+void siftDown(HEAP* heap, int index);
 void decreaseKey(HEAP* heap, int index, int new_cost);
